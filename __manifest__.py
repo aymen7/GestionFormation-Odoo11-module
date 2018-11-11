0c8e2288e28aab557e11222200376f825e1e4af1
Aymen Bennour <aymenokay@gmail.com>
2018-11-11 15:00:50 +0100

    sample demo working

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "gestion_formation",
+    'name': "Gestion des formations.",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Ce module est destiné pour gérer un centre de formation avec Odoo11. 
+        """,
 
     'description': """
-        Long description of module's purpose
+        Ce module est destiné pour gérer un centre de formation.
     """,
 
-    'author': "Aymen Bennour, Malti Mohammed",
+    'author': "Aymen Bennour",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
